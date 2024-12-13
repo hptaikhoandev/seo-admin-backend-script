6c01479bde86e044456b00ef8d99a29f0ca10748
etingvn <ting@darasa.io>
2024-12-13 17:34:21 +0800

    add start restart stop to server

--- app/models/server_request.py
+++ app/models/server_request.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from typing import List
+from typing import Optional
 
 class ServerRequest(BaseModel):
     team: str
     server_ip: str
+    transitions: Optional[str] = None
